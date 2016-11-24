7e28067a9b5276a79e6038c4d6a24deeeddd2bfe
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-11-24 13:21:44 -0500

    changed size of initial seed and test data (easy -> moderate)

--- test_script.m
+++ test_script.m
@@ -1,21 +1,26 @@
 %% test random learner
 
 % load easy data
-filename = 'Data/EASY_TRAIN.csv';
-test_filename = 'Data/EASY_TEST.csv';
+% filename = 'Data/EASY_TRAIN.csv';
+% test_filename = 'Data/EASY_TEST.csv';
+filename = 'Data/MODERATE_TRAIN.csv';
+test_filename = 'Data/MODERATE_TEST.csv';
+% filename = 'Data/DIFFICULT_TRAIN.csv';
+% test_filename = 'Data/DIFFICULT_TEST.csv';
+
 [easy_x, easy_y, encoding] = read_mydata(filename);
 [easy_test_x, easy_test_y, ~] = read_mydata(filename);
 
 %% random learner
 buget = 450;
 report_step = 20;
-initial_seed = 20; % initialize with 20 samples randomly selected from the pool
-base_learner_random = learner_libsvm(0, 0);
+initial_seed = 10; % initialize with 20 samples randomly selected from the pool
+base_learner_random = learner_libsvm(2, 0);
 [model_random, errors_random, costs_random] = random_learner(easy_x, easy_y, buget, ...
     easy_test_x, easy_test_y, base_learner_random, report_step, initial_seed);
 
 %% uncertainty learner 
-base_learner_uncertainty = learner_libsvm(0, 1);
+base_learner_uncertainty = learner_libsvm(2, 1);
 [model_uncertainty, errors_uncertainty, costs_uncertainty] = uncertainty_based_learner(easy_x, easy_y, buget, ...
     easy_test_x, easy_test_y, base_learner_uncertainty, report_step, initial_seed);
 
