57074f2869297449494a4a8ce2ae3313d3613026
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-12-03 20:52:01 -0500

    updated read data function

--- read_mydata.m
+++ read_mydata.m
@@ -1,7 +1,7 @@
-function [data, labels, encoding] = read_mydata(filename)
+function [data, labels, encoding] = read_mydata(filename, ncol)
     temp = readtable(filename, 'Delimiter',',', 'ReadVariableNames', false);
-    data = table2array(temp(:, 1:26));
-    labels = table2array(temp(:, 27));
+    data = table2array(temp(:, 1:ncol));
+    labels = table2array(temp(:, ncol + 1));
     encoding = sort(unique(labels));
     temp_index = 1 : size(encoding, 1);
     labels = cellfun(@(x) temp_index(strcmp(encoding, x) == 1), labels);
--- test_script.m
+++ test_script.m
@@ -1,38 +1,41 @@
 %% test random learner
 
 % load easy data
-filename = 'Data/EASY_TRAIN.csv';
-test_filename = 'Data/EASY_TEST.csv';
-% filename = 'Data/MODERATE_TRAIN.csv';
-% test_filename = 'Data/MODERATE_TEST.csv';
-% filename = 'Data/DIFFICULT_TRAIN.csv';
-% test_filename = 'Data/DIFFICULT_TEST.csv';
+% filename = 'Data/EASY_TRAIN.csv';
+% test_filename = 'Data/EASY_TEST.csv';
+% ncol = 26;
+filename = 'Data/MODERATE_TRAIN.csv';
+test_filename = 'Data/MODERATE_TEST.csv';
+ncol = 26;
+% filename = 'Data/DIFFICULT_TRAIN_conv.csv';
+% test_filename = 'Data/DIFFICULT_TEST_conv.csv';
+% ncol = 52;
 
-[easy_x, easy_y, encoding] = read_mydata(filename);
-[easy_test_x, easy_test_y, ~] = read_mydata(test_filename);
+[easy_x, easy_y, encoding] = read_mydata(filename, ncol);
+[easy_test_x, easy_test_y, ~] = read_mydata(test_filename, ncol);
 
 %% random learner + libsvm
 buget = 2500;
-% report_step = 20;
-% initial_seed = 10; % initialize with 20 samples randomly selected from the pool
-% base_learner_random = learner_libsvm(2, 0);
-% [model_random, errors_random, costs_random] = random_learner(easy_x, easy_y, buget, ...
-%     easy_test_x, easy_test_y, base_learner_random, report_step, initial_seed);
+report_step = 20;
+initial_seed = 10; % initialize with 20 samples randomly selected from the pool
+base_learner_random = learner_libsvm(2, 0);
+[model_random, errors_random, costs_random] = random_learner(easy_x, easy_y, buget, ...
+    easy_test_x, easy_test_y, base_learner_random, report_step, initial_seed);
 
 %% uncertainty learner + libsvm
-% base_learner_uncertainty = learner_libsvm(2, 1);
-% [model_uncertainty, errors_uncertainty, costs_uncertainty] = uncertainty_based_learner(easy_x, easy_y, buget, ...
-%     easy_test_x, easy_test_y, base_learner_uncertainty, report_step, initial_seed);
-
-%% random learner + sparselogit
-base_learner_sparselogit = learner_sparselogit(100, 100, 1e-4);
-[model_random, errors_random, costs_random] = random_learner(easy_x, easy_y, buget, ...
-    easy_test_x, easy_test_y, base_learner_sparselogit, report_step, initial_seed);
-
-%% random learner + sparselogit
-base_learner_sparselogit = learner_sparselogit(100, 100, 1e-4);
+base_learner_uncertainty = learner_libsvm(2, 1);
 [model_uncertainty, errors_uncertainty, costs_uncertainty] = uncertainty_based_learner(easy_x, easy_y, buget, ...
-    easy_test_x, easy_test_y, base_learner_sparselogit, report_step, initial_seed);
+    easy_test_x, easy_test_y, base_learner_uncertainty, report_step, initial_seed);
+
+%% random learner + sparselogit
+% base_learner_sparselogit = learner_sparselogit(100, 100, 1e-4);
+% [model_random, errors_random, costs_random] = random_learner(easy_x, easy_y, buget, ...
+%     easy_test_x, easy_test_y, base_learner_sparselogit, report_step, initial_seed);
+
+%% random learner + sparselogit
+% base_learner_sparselogit = learner_sparselogit(100, 100, 1e-4);
+% [model_uncertainty, errors_uncertainty, costs_uncertainty] = uncertainty_based_learner(easy_x, easy_y, buget, ...
+%     easy_test_x, easy_test_y, base_learner_sparselogit, report_step, initial_seed);
 
 %% plot results
 figure;
